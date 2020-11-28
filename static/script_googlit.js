fa2e4477259d7cd533cd2dbfd6201f79e926cfb2
Emre Küçükkaya <kardandon@gmail.com>
2020-11-28 22:44:10 +0300

    Add files via upload

---
+++ static/script_googlit.js
@@ -0,0 +1,36 @@
+var getParams = function (url) {
+	var params = {};
+	var parser = document.createElement('a');
+	parser.href = url;
+	var query = parser.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		params[pair[0]] = decodeURIComponent(pair[1]);
+	}
+	return params;
+};
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+img = new Image();
+img.src = "/static/googleit_logo.png";
+img.onload = function(){
+	document.querySelector('#googlitimg').src = img.src;
+};
+window.onload = async function(){
+	qwerty = getParams(window.location.href);
+	console.log(qwerty['q']);
+	if (!qwerty['q']) 
+	{
+		qwerty['q'] = "emrekucukkaya.github.io";
+	}
+	param = document.querySelector('input');
+	param.value = "";
+	for (let i = 0; i < qwerty['q'].length; i++)
+	{
+		param.value += qwerty['q'][i];
+		await sleep(200);
+	}
+	document.querySelector('form').submit();
+};
